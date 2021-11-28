93e0ea575d3eae22e0efae52a7a9ea1114c63609
Rittr <chrisritter2@gmail.com>
2021-11-28 18:01:01 -0500

    added random number generation for each dice roll

--- rng_flutter/lib/dice.dart
+++ rng_flutter/lib/dice.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_grid_button/flutter_grid_button.dart';
 
@@ -14,81 +16,19 @@ class DiceRoute extends StatelessWidget {
       body: Column(children: [
         // add table of dice buttons here
         Expanded(
-          child:
-              // GridButton(
-              //   textStyle: textStyle,
-              //   borderColor: Colors.grey[300],
-              //   borderWidth: 2,
-              //   onPressed: (dynamic val) {
-              //     ScaffoldMessenger.of(context).showSnackBar(
-              //       SnackBar(
-              //         content: Text(val.toString()),
-              //         duration: Duration(milliseconds: 400),
-              //       ),
-              //     );
-              //   },
-              //   items: [
-              //     [
-              //       GridButtonItem(
-              //         title: "Black",
-              //         color: Colors.black,
-              //         textStyle: textStyle.copyWith(color: Colors.white),
-              //       ),
-              //       GridButtonItem(
-              //         title: "Red",
-              //         color: Colors.red,
-              //         textStyle: textStyle.copyWith(color: Colors.white),
-              //       ),
-              //     ],
-              //     [
-              //       GridButtonItem(
-              //           child: Icon(
-              //             Icons.image_outlined,
-              //             size: 50,
-              //           ),
-              //           textStyle: textStyle.copyWith(color: Colors.white),
-              //           value: 'image',
-              //           color: Colors.blue,
-              //           shape: BorderSide(width: 4),
-              //           borderRadius: 30)
-              //     ],
-              //     [
-              //       GridButtonItem(title: "7"),
-              //       GridButtonItem(title: "8"),
-              //       GridButtonItem(title: "9"),
-              //       GridButtonItem(title: "×", color: Colors.grey[300]),
-              //     ],
-              //     [
-              //       GridButtonItem(title: "4"),
-              //       GridButtonItem(title: "5"),
-              //       GridButtonItem(title: "6"),
-              //       GridButtonItem(title: "-", color: Colors.grey[300]),
-              //     ],
-              //     [
-              //       GridButtonItem(title: "1"),
-              //       GridButtonItem(title: "2"),
-              //       GridButtonItem(title: "3"),
-              //       GridButtonItem(title: "+", color: Colors.grey[300]),
-              //     ],
-              //     [
-              //       GridButtonItem(title: "0"),
-              //       GridButtonItem(title: "000", flex: 2, longPressValue: 400),
-              //       GridButtonItem(title: "=", color: Colors.grey[300]),
-              //     ],
-              //   ],
-              // ),
-
-              GridButton(
+          child: GridButton(
             onPressed: (var val) {
+              Random random = Random();
+              var rnd = random.nextInt(val)+1;
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
-                  content: Text(val.toString()),
+                  content: Text(rnd.toString()),
                   duration: const Duration(milliseconds: 400),
                 ),
               );
             },
 
-            // onPressed: () {
+            // onPressed: (var value) {
             //   // rollDice(value);
             // },
             items: const [
@@ -101,7 +41,6 @@ class DiceRoute extends StatelessWidget {
                 GridButtonItem(title: "D8", value: 8),
                 GridButtonItem(title: "D10", value: 10),
                 GridButtonItem(title: "D12", value: 12),
-
               ],
               [
                 GridButtonItem(title: "D20", value: 20),
